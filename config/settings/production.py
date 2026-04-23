be76ec70045cc9e8c174a5848bfc879e160602a6
carolinanogs <carolnogueira2807@gmail.com>
2026-04-23 18:59:11 -0400

    corrige database url railway

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,13 +8,13 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL não encontrada no Railway")
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': dj_database_url.parse(DATABASE_URL)
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
-
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
